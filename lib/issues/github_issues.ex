8942c7aa674337f89bdd2bf47a87b8718d253deb
Christopher Eyre <chriseyre2000@gmail.com>
2017-06-28 09:49:36 +0100

    Adding Poison to parse json response

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,6 @@
 defmodule Issues.GithubIssues do
   @user_agent "Elixir Issues"
+  @github_url Application.get_env(:issues, :github_url)
   
   def fetch(user, project) do
     issues_url(user, project) 
@@ -8,15 +9,15 @@ defmodule Issues.GithubIssues do
   end
   
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
   
   def handle_response({%{status_code: 200, body: body}}) do
-    {:ok, body}
+    {:ok, Poison.Parser.parse!(body)}
   end
   
   def handle_response({%{status_code: _, body: body}}) do
-    {:error, body}
+    {:error, Poison.Parser.parse!(body)}
   end
 
 end
--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-	 {:httpotion, "~> 3.0.2"}
+	 httpotion: "~> 3.0.2",
+	 poison: "~> 2.2"
 	]
   end
 end
